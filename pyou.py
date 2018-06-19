b48f5d164e8d531b1d6aeeeba2a901cc63c01c6b
davy1ex <32157455+davy1ex@users.noreply.github.com>
2018-06-19 18:55:34 +0400

    Update pyou.py

--- pyou.py
+++ pyou.py
@@ -19,13 +19,6 @@ if __name__ == '__main__':
 	parser = createParser()
 	namespace = parser.parse_args()
 
-	# if namespace.llnk:
-	# 	try:
-	# 		yt = YouTube(namespace.link)
-	# 	except RegexMatchError:
-	# 		print("ERROR: The intered URL is incorrect")
-	# else:
-	# 	raise "ERROR: link was not specified"
 	try:
 		yt = YouTube(namespace.link)
 	except RegexMatchError:
@@ -46,13 +39,10 @@ if __name__ == '__main__':
 
 	if namespace.path:
 		path = os.path.join(os.getcwd(), namespace.path)
-		# name = yt[0].default_filename
-		#print(name, "will save here:", path)
 	else:
 
 		path = os.path.join(os.getcwd(), "Downloads")
-		# print(name, "will save here:", path)
-	
+
 	print("Downloading...")
 	start_time = time.time()
 	try:
@@ -62,14 +52,12 @@ if __name__ == '__main__':
 	yt[0].download(path)
 	
 	if namespace.format == "mp3":
-		#name = yt[0].default_filename
 		print("Formating...")
 		new_name = name.replace(name.split(".")[1], "mp3")
 		try:
 			os.rename(os.path.join(path, name), os.path.join(path, new_name))
 		except FileExistsError:
 			print("File already exists")
-	# print("--- %s seconds ---" % (time.time() - start_time))
 	time = time.time() - start_time
 	print(time)
-	print("Finished with", str(time.time() - start_time)[0:3], "Sec.")
+	print("Finished with", str(time.time() - start_time))[0:3], "Sec.")
